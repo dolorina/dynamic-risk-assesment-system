6f4e15d3eb62da7fa8bdbca84d497eb44b97dfcf
dolorina <marinadolokov@gmx.de>
2022-02-08 19:09:57 +0100

    current status of deployment.py

--- starter-file/deployment.py
+++ starter-file/deployment.py
@@ -10,15 +10,16 @@ import json
 
 
 
-##################Load config.json and correct path variable
+# Load config.json and correct path variable
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'])
 prod_deployment_path = os.path.join(config['prod_deployment_path']) 
+model_path =  os.path.join(config['output_model_path']) 
 
 
-####################function for deployment
+# Function for deployment
 def store_model_into_pickle(model):
     #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
         
